2cfaa25bd7e6b20c043168b391658b9ab570e5ba
Jayesh Kitukale <jayeshkitukale@Jayeshs-Mac-mini.local>
2014-12-10 12:51:12 +0530

    Initial Commit

---
+++ SnatchIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnatchIt
+//
+//  Created by Jayesh Kitukale on 12/10/14.
+//  Copyright (c) 2014 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnatchItTests/SnatchItTests.m
@@ -0,0 +1,40 @@
+//
+//  SnatchItTests.m
+//  SnatchItTests
+//
+//  Created by Jayesh Kitukale on 12/10/14.
+//  Copyright (c) 2014 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SnatchItTests : XCTestCase
+
+@end
+
+@implementation SnatchItTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
