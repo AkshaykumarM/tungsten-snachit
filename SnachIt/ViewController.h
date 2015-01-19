5d000e16e2d99a61ebbf11b0babf9054b0743bb1
Jayesh Kitukale <jayeshkitukale@Jayeshs-Mac-mini.local>
2015-01-19 13:06:51 +0530

    Initial Commit

---
+++ SnachIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnachIt
+//
+//  Created by Jayesh Kitukale on 1/19/15.
+//  Copyright (c) 2015 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SnachIt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SnachIt
+//
+//  Created by Jayesh Kitukale on 1/19/15.
+//  Copyright (c) 2015 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
